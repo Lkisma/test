890828bc202add2920de618fed073632d7da1573
Linda Kisma <lindakisma@MacBook-Air-de-Linda.local>
2020-09-30 13:52:03 +0200

    premier push

---
+++ Eco.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>L'éco-conception</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap" rel="stylesheet">
+    
+
+  </head>
+  <body>
+    <!--part1 : Titre et logo-->
+  <div class="container-logo">
+      <img class="logotext" id="logo" src="images/eco-vert.jpeg" alt="eco vert">
+      <h1 class="textlogo">L’éco-conception</h1>
+    </div>
+
+      <!--part2 Titre Constat et text-->
+   <div class="bloc-div"> 
+      <div><h2 class="titre-constat">Le constat</h2>
+      <p >Le web pollue de manière croissante et inquiétante, il faut donc concevoir des sites web plus légers et sollicitant moins d'allers-retours serveur(s). </p>
+      <p >L’impact environnemental du numérique n’est pas toujours connu, pourtant il est très important. </p>
+       <p>Pour s’en rendre compte, voici quelques chiffres :</p><br/></br>
+      </div>
+  </div>
+       
+      <!--part3 Cerle chiffre: -->
+       
+  <div id="bloc" >
+       <div class="column1"><h3>15000 kilomètres</h3></br>sont en moyenne parcourus pour une donnée numérique </div> 
+       <div class="column1"><h3>7 grammes de CO2 </h3></br>sont en moyenne parcourus pour une donnée numérique  sont émis lors d’une recherche Google</div> 
+       <div class="column1"><h3>10 % de l’électricité </h3></br>sont consommées dans notre pays par les data center français.</div> 
+    </div>  
+        <!--part4 Déf Eco conception-->
+    <div class="eco-image">
+      <h2>Qu’est-ce que l’éco-conception ?</h2>
+      <p>L'Eco-conception c'est l'intégration de la réduction des impacts environnementaux dès la phase de conception d'un service numérique avec une vision globale sur l'ensemble du cycle de vie. Le même service rendu génère moins d'impacts s'il est éco-conçu. En intégrant les dimensions sociale et économique, elle mène à la conception numérique responsable ".</p>
+       
+       <p>Il faut donc réduire l'impact environnemental du web tout en gagnant en performance. </p>
+       
+       <!--part6 image cycle de vie-->  
+        <p>L’éco-conception se doit intervenir dans chaque étape du cycle d’un produit, et non seulement dans quelques étapes.</p>
+      </div></br> </br>
+        
+      <img  class="imagecycle"src="images/cycle-vie.jpg" alt="Cycle de vie" ></br>
+        
+     <!--part6 vidéo YT-->  
+    <div class="videoYT">
+      <div class="textvideo">Retrouver dans cette vidéo l’explication de l'éco-conception selon une agence qui la pratique lors de toute ses conceptions.</div>
+      <div class="imagevidéo"><iframe width="560" height="315" src="https://www.youtube.com/embed/790WTNgg-78" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>  
+       
+    </div >  
+
+     <!--part6 conclusion renvoi vers les bonnes pratiques-->
+    <div class="conclusion">
+       <span class="fin"><div class="textconclusion">Et vous ? Avez-vous déjà réfléchi à l’empreinte écologique de votre site web ? </div></span>
+       <div class="textconclusion"  >Dans le numérique, comme dans d’autres secteurs, la tendance est au vert. De plus en plus d’entreprises se mettent à l’éco-conception web et cherchent à trouver le bon compromis entre performance et impact environnemental.Dans la page suivante, nous passons au crible l’éco-conception web et ses bonnes pratiques. Découvrez quelles sont les bonnes pratiques, simples à mettre en place rendre votre site Internet moins gourmand en ressources énergétiques.
+       </div></span>
+       <div class="boxbutton"><button class="box">En savoir plus</button></div>
+   
+    </div>
+  
+  </body>
+</html>
---
+++ app.js
@@ -0,0 +1,2 @@
+ScrollReveal({ distance: '60px' }); ScrollReveal().reveal('.h2', { origin: 'top' }); ScrollReveal().reveal('.tagline', { origin: 'bottom' });
+
